922af10acf685725d9566822585c7ceb18e44adb
Sammy <sam.fastlane@gmail.com>
2016-07-15 11:21:27 -0500

    Theme struct
    Created basic theme structure

---
+++ front-page.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Front page template
+ *
+ * @package 	Sandbox
+ * @author 		Ephraim Wickline
+ * @link 		http://caresandbox.wpengine.com
+ * @copyright	Copyright (c) 2016, Ephraim Wickline
+ * @license		GPL-3.0+
+ */
+
+genesis();
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Theme customizations
+ *
+ * @package 	Sandbox
+ * @author 		Ephraim Wickline
+ * @link 		http://caresandbox.wpengine.com
+ * @copyright	Copyright (c) 2016, Ephraim Wickline
+ * @license		GPL-3.0+
+ */
+
+//load child theme textdomain.
+load_child_theme_textdomain( 'sandbox' );
+
+add_action( 'genesis_setup', 'sandbox_setup');
+
+/**
+ * Theme Setup
+ *
+ * Attach all of the site-wide functions to the correct hooks and filters. All the
+ * functions themselves are below this setup function.
+ *
+ * @since 	1.0.0
+ *
+ */
+
+function scratch_setup() {
+
+
+
+}
